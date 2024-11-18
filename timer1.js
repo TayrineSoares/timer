d3ded46a295d6c8b801944b8e40586ffcd5cd916
TayrineSoares <tayrinecristina@hotmail.com>
2024-11-18 13:37:24 -0500

    Changed the Fixed the code and addded comments

--- timer1.js
+++ timer1.js
@@ -6,13 +6,14 @@ const sliced = args.slice(2); // retrieving values from the command line
 const alarm = function (numbers) {
   for (let items of numbers) {
     const delay = Number(items); //Number converts items to a number
-    if (!isNaN(delay) && delay > 0) { // making sure the argument is a positive number
+    if (!isNaN(delay) && delay > 0) { // making sure delay is a positive number
 
       setTimeout(() => {
-        process.stdout.write("\x07");}, delay * 1000);
+        process.stdout.write("\x07"); // beep 
+      }, delay * 1000); // 
       }
     }
   };
 
-alarm(args); 
+alarm(sliced); 
 
