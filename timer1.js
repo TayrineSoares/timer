25241124d4718577076b83413cca92ed0ec1caec
TayrineSoares <tayrinecristina@hotmail.com>
2024-11-18 13:28:16 -0500

    Timer

---
+++ timer1.js
@@ -0,0 +1,18 @@
+// Implement an alarm clock / timer which will beep after a specified amount of time has passed. The user can specify an unlimited number of alarms using command line arguments
+
+const args = process.argv;
+const sliced = args.slice(2); // retrieving values from the command line
+
+const alarm = function (numbers) {
+  for (let items of numbers) {
+    const delay = Number(items); //Number converts items to a number
+    if (!isNaN(delay) && delay > 0) { // making sure the argument is a positive number
+
+      setTimeout(() => {
+        process.stdout.write("\x07");}, delay * 1000);
+      }
+    }
+  };
+
+alarm(args); 
+
